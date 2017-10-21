f67027fed9acb6fad843e2e729a4f920a22e23ff
quydx <quy196hp8@gmail.com>
2017-10-21 22:32:44 +0700

    modify selectsort.py

--- code/selectionSort.py
+++ code/selectionSort.py
@@ -1,6 +1,11 @@
 def selection_sort(array):
-
-	return array
+	source = array
+	for i in range(len(source)):
+	  mini = min(source[i:]) #find minimum element
+	  min_index = source[i:].index(mini)-1 #find index of minimum element
+	  source[i:][min_index]= source[i:][0] #replace element at min_index with first element
+	  source[i:][0] = mini                  #replace first element with min element
+	return source
 	
 if __name__ == "__name__":
 ##
